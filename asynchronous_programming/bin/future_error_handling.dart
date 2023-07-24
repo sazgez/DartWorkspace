c03b5ab38dde4bf26a943619d072d2ddb075caeb
sazgez <wishell.rock.4@gmail.com>
2023-07-24 17:40:17 +0300

    Progress has been made on the 15. topic
    Asynchronous Programming

---
+++ asynchronous_programming/bin/future_error_handling.dart
@@ -0,0 +1,30 @@
+void main(List<String> args) async {
+  try {
+    print(await getFullName(
+      firstName: 'John',
+      lastName: 'Doe',
+    ));
+
+    print(await getFullName(
+      firstName: 'John',
+      lastName: '',
+    ));
+  } on FirstOrLastNameMissingException {
+    print('First or last name is missing');
+  }
+}
+
+Future<String> getFullName({
+  required String firstName,
+  required String lastName,
+}) {
+  if (firstName.isEmpty || lastName.isEmpty) {
+    throw FirstOrLastNameMissingException();
+  } else {
+    return Future.value('$firstName $lastName');
+  }
+}
+
+class FirstOrLastNameMissingException implements Exception {
+  const FirstOrLastNameMissingException();
+}
